cdd165ba5591ba943aad5a6d20f1361632c391ab
DIR\vicente.ruiz.salcido <raul.x.ruiz.-nd@disney.com>
2017-04-22 23:43:56 -0400

    Improve board and tile classes

--- app/src/main/java/com/raul311/chess/board/Board.java
+++ app/src/main/java/com/raul311/chess/board/Board.java
@@ -1,6 +1,7 @@
 package com.raul311.chess.board;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.GridView;
 
 /**
@@ -9,9 +10,33 @@ import android.widget.GridView;
 
 public class Board extends GridView {
 
+    private static final int COLS = 8;
+    private static final int ROWS = 8;
+
+    private Tile[][] tiles;
+    private int x = 0;
+    private int y = 0;
+    private int squareSize = 0;
+
+    //private Context context;
+
     public Board(Context context) {
         super(context);
+        createTiles(context);
     }
 
+    public Board(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        createTiles(context);
+    }
+
+    private void createTiles(Context context) {
+        this.tiles = new Tile[COLS][ROWS];
+        for (int column = 0; column < COLS; column++) {
+            for (int row = 0; row < ROWS; row++) {
+                tiles[column][row] = new Tile(context);
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/raul311/chess/board/Tile.java
+++ app/src/main/java/com/raul311/chess/board/Tile.java
@@ -1,7 +1,10 @@
-package com.raul311.chess.chessboard;
+package com.raul311.chess.board;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
+
+import com.raul311.chess.pieces.ChessPiece;
 
 /**
  * Created by raul311
@@ -9,9 +12,36 @@ import android.view.View;
 
 public class Tile extends View {
 
+    private Color color;
+    private ChessPiece chessPiece;
+    private int position;
+
     public Tile(Context context) {
         super(context);
     }
 
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setChessPiece(ChessPiece chessPiece) {
+        this.chessPiece = chessPiece;
+    }
+
+    public ChessPiece getChessPiece() {
+        return chessPiece;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 
 }
